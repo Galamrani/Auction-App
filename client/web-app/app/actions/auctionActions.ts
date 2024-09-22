3fcec20093ab91f306e071dc6e4061debd486828
Gal amrani <galamrani2@gmail.com>
2024-09-22 19:40:01 +0300

    Deploy all images fix

--- client/web-app/app/actions/auctionActions.ts
+++ client/web-app/app/actions/auctionActions.ts
@@ -42,3 +42,5 @@ export async function getBidsForAuction(id: string): Promise<Bid[]> {
 export async function placeBidForAuction(auctionId: string, amount: number) {
     return await fetchWrapper.post(`bids?auctionId=${auctionId}&amount=${amount}`, {})
 }
+
+
