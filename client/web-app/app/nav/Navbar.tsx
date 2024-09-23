0f2dff28130f1e425d5519daf0c9b6d73c597f70
Gal amrani <galamrani2@gmail.com>
2024-09-23 11:51:41 +0300

    Deploy to DO

--- client/web-app/app/nav/Navbar.tsx
+++ client/web-app/app/nav/Navbar.tsx
@@ -4,7 +4,7 @@ import LoginButton from './LoginButton';
 import { getCurrentUser } from '../actions/authActions';
 import UserActions from './UserActions';
 
-export default async function Navbar() {
+export default async function NavBar() {
     const user = await getCurrentUser();
     return (
         <header className='sticky top-0 z-50 flex justify-between bg-white shadow-md py-5 px-5 items-center text-gray-800'>
